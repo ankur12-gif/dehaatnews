e4e4bd199a30d91a66e8f43a4c0b982c5aa9575f
Ankur Sharma <ankur11murhu@gmail.com>
2025-05-31 00:37:00 +0530

    commit

--- server/src/controllers/post.js
+++ server/src/controllers/post.js
@@ -12,16 +12,23 @@ const createPost = TryCatch(async (req, res) => {
     const { title, description, category } = req.body;
     const photos = req.files;
 
-    if (!photos) return next(new Error("Please upload photos", 400));
-    if (!title || !description || !category) return next(new Error("Please enter all fields"));
+    console.log({title})
 
-    const photosUrl = await uploadToImageKit(photos);
+    // if (!photos) return next(new Error("Please upload photos", 400));
+    // if (!title || !description || !category) return next(new Error("Please enter all fields"));
+
+    // const photosUrl = await uploadToImageKit(photos);
 
     const post = await Posts.create({
         title,
         description,
         category,
-        photos: photosUrl,
+        photos: [
+            {
+                public_id: 'hedsdsd', // ✅ ImageKit uses fileId instead of public_id
+                url: "https://ik.imagekit.io/cxa7ojrtpq/uploads/1_Yj9KgxMor.jpg",
+            }
+        ],
     });
 
     myCache.del("allPosts");
@@ -69,10 +76,10 @@ const getSinglePost = TryCatch(async (req, res, next) => {
         return res.status(200).json({ success: true, post: cachedPost });
     }
 
-    const post = await Posts.findById(postId);
+    const post = await Posts.findById("6836ee28071f223f25d6331c");
     if (!post) return next(new Error("Post does not exist", 400));
 
-    myCache.set(`post_${postId}`, post, TTL);
+    myCache.set(`post_${"6836ee28071f223f25d6331c"}`, post, TTL);
     return res.status(200).json({ success: true, post });
 });
 
